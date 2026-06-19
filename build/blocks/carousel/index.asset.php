b8023932bf90ef376cec1a91058d4579d29d3bc8
Alex Standiford <alex@novatori.us>
2026-06-19 07:35:16 -0400

    Build responsive carousel direction controls

--- build/blocks/carousel/index.asset.php
+++ build/blocks/carousel/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '2d4a4270804b11b10cbc');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '482a1c9390b8af3145c7');
--- build/blocks/carousel/view.asset.php
+++ build/blocks/carousel/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '871963dc09ef630b6e01');
+<?php return array('dependencies' => array(), 'version' => '73e39a1e1ca8e35a6e09');
